c3e85e78f283abbf986a733fcf13d067cc78c71d
Mohamed <mouh4945@gmail.com>
2025-06-15 14:39:13 +0100

    update

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,5 +1,14 @@
 import { Tabs } from "expo-router";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
+}) {
+  return <FontAwesome size={24} style={{ marginBottom: 4 }} {...props} />;
+}
 
 const TabsLayout = () => {
   return (
@@ -12,9 +21,13 @@ const TabsLayout = () => {
           tabBarStyle: {
             backgroundColor: "white",
             height: 70,
+            borderTopWidth: 0,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingBottom: 4,
           },
           tabBarLabelStyle: {
-            fontSize: 14,
+            fontSize: 13,
           },
           tabBarIconStyle: {
             paddingTop: 4,
@@ -24,11 +37,21 @@ const TabsLayout = () => {
       >
         <Tabs.Screen
           name="index"
-          options={{ title: "Home", headerShown: false }}
+          options={{
+            title: "shop",
+            tabBarIcon(props) {
+              return <TabBarIcon name="shopping-cart" {...props} />;
+            },
+          }}
         />
         <Tabs.Screen
           name="orders"
-          options={{ title: "Orders", headerShown: false }}
+          options={{
+            title: "Orders",
+            tabBarIcon(props) {
+              return <TabBarIcon name="book" {...props} />;
+            },
+          }}
         />
       </Tabs>
     </SafeAreaView>
--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Hello ........ </Text>
+      <Text>/ Home </Text>
     </View>
   );
 }
