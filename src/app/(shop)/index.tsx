284aea041eabdb32098ba96697341176f3ddd371
Mohamed <mouh4945@gmail.com>
2025-06-16 18:23:04 +0100

    update

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,17 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StatusBar, StyleSheet, Text, View } from "react-native";
+
+import { PRODUCTS } from "../../../assets/products";
+import { productListItem } from "../../components/product-List-Item";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>/ Home </Text>
+    <View>
+      {/* <Text>/ Home </Text> */}
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text> {item.title} </Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text> Product</Text>}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{
+          width: "100%",
+          paddingHorizontal: 10,
+          paddingVertical: 5,
+        }}
+      >
+        {" "}
+      </FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  flatListContent: {
+    paddingBlock: 20,
+  },
+  flatListColumn: {
+    // paddingVertical: 20,
+    justifyContent: "space-between",
   },
 });
---
+++ src/app/components/product-list-item.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function productListItem() {
+  return (
+    <View>
+      <Text>product-list-item</Text>
+    </View>
+  );
+}
